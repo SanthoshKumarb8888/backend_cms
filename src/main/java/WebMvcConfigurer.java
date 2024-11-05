e4cc501c795dd7f3171afccb2f79c7fd81de3939
santhosh <santhoshkumarb8888@gmail.com>
2024-11-05 13:06:46 +0530

    ok2

--- src/main/java/WebMvcConfigurer.java
+++ src/main/java/WebMvcConfigurer.java
@@ -4,12 +4,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class backend_cms WebMvcConfigurer.java implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Applies to all endpoints
-                .allowedOrigins("http://44.203.120.219:3000") // Allow your frontend origin
+                .allowedOrigins("*) // Allow your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed HTTP methods
                 .allowCredentials(true); // If using credentials like cookies or headers
     }
--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "http://44.203.120.219:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/employees")
 @RestController
 public class EmployeeController {
